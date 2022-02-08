synfinatic/aws-sso-cli

commit 41e96526e58a53d68ce82199e09d90ec819a6910

Support specifying the profile name for role
 * Add support for specifying the role to assume via the
    -p/--profile argument
 * Console flag -p => -P

Fixes: #268

cmd/process_cmd.go
@@ -33,6 +33,7 @@ type ProcessCmd struct {
 	Arn       string `kong:"short='a',help='ARN of role to assume',xor='arn-1',xor='arn-2',predictor='arn'"`
 	AccountId int64  `kong:"name='account',short='A',help='AWS AccountID of role to assume',xor='arn-1',predictor='accountId'"`
 	Role      string `kong:"short='R',help='Name of AWS Role to assume',xor='arn-2',predictor='role'"`
+	Profile   string `kong:"short='p',help='Name of AWS Profile to assume',predictor='profile'"`
 }
 
 func (cc *ProcessCmd) Run(ctx *RunContext) error {
@@ -45,14 +46,23 @@ func (cc *ProcessCmd) Run(ctx *RunContext) error {
 	role := ctx.Cli.Process.Role
 	account := ctx.Cli.Process.AccountId
 
-	if len(ctx.Cli.Process.Arn) > 0 {
+	if ctx.Cli.Process.Profile != "" {
+		cache := ctx.Settings.Cache.GetSSO()
+		rFlat, err := cache.Roles.GetRoleByProfile(ctx.Cli.Eval.Profile, ctx.Settings)
+		if err != nil {
+			return err
+		}
+
+		role = rFlat.RoleName
+		account = rFlat.AccountId
+	} else if ctx.Cli.Process.Arn != "" {
 		account, role, err = utils.ParseRoleARN(ctx.Cli.Process.Arn)
 		if err != nil {
 			return err
 		}
 	}
 
-	if len(role) == 0 || account == 0 {
+	if role != "" || account == 0 {
 		return fmt.Errorf("Please specify --arn or --account and --role")
 	}
 

cmd/complete.go
@@ -26,6 +26,7 @@ import (
 	// "github.com/davecgh/go-spew/spew"
 	"github.com/goccy/go-yaml"
 	"github.com/posener/complete"
+	log "github.com/sirupsen/logrus"
 	"github.com/synfinatic/aws-sso-cli/sso"
 	"github.com/synfinatic/aws-sso-cli/utils"
 )
@@ -35,6 +36,7 @@ type Predictor struct {
 	accountids []string
 	roles      []string
 	arns       []string
+	profiles   []string
 }
 
 // AvailableAwsRegions lists all the AWS regions that AWS provides
@@ -91,13 +93,19 @@ func NewPredictor(cacheFile, configFile string) *Predictor {
 	uniqueRoles := map[string]bool{}
 
 	cache := c.GetSSO()
-	//	fmt.Printf("cache: %s", spew.Sdump(c))
-	for i, a := range cache.Roles.Accounts {
-		id, _ := utils.AccountIdToString(i)
+	for aid := range cache.Roles.Accounts {
+		id, _ := utils.AccountIdToString(aid)
 		p.accountids = append(p.accountids, id)
-		for role, r := range a.Roles {
-			p.arns = append(p.arns, r.Arn)
-			uniqueRoles[role] = true
+
+		for roleName, rFlat := range cache.Roles.GetAccountRoles(aid) {
+			p.arns = append(p.arns, rFlat.Arn)
+			uniqueRoles[roleName] = true
+			profile, err := rFlat.ProfileName(settings)
+			if err != nil {
+				log.Warnf(err.Error())
+				continue
+			}
+			p.profiles = append(p.profiles, profile)
 		}
 	}
 
@@ -159,4 +167,17 @@ func (p *Predictor) SsoComplete() complete.Predictor {
 		}
 	}
 	return complete.PredictSet(ssos...)
+}
+
+// ProfileComplete returns a list of all the valid AWS_PROFILE values
+func (p *Predictor) ProfileComplete() complete.Predictor {
+	profiles := []string{}
+
+	// The `:` character is considered a word delimiter by bash complete
+	// so we need to escape them
+	for _, x := range p.profiles {
+		profiles = append(profiles, strings.ReplaceAll(x, ":", "\\:"))
+	}
+
+	return complete.PredictSet(profiles...)
 }

cmd/console_cmd.go
@@ -42,11 +42,12 @@ type ConsoleCmd struct {
 	// Console actually should honor the --region flag
 	Region   string `kong:"help='AWS Region',env='AWS_DEFAULT_REGION',predictor='region'"`
 	Duration int64  `kong:"short='d',help='AWS Session duration in minutes (default 60)'"` // default stored in DEFAULT_CONFIG
-	Prompt   bool   `kong:"short='p',help='Force interactive prompt to select role'"`
+	Prompt   bool   `kong:"short='P',help='Force interactive prompt to select role'"`
 
 	Arn       string `kong:"short='a',help='ARN of role to assume',env='AWS_SSO_ROLE_ARN',predictor='arn'"`
 	AccountId int64  `kong:"name='account',short='A',help='AWS AccountID of role to assume',env='AWS_SSO_ACCOUNT_ID',predictor='accountId'"`
 	Role      string `kong:"short='R',help='Name of AWS Role to assume',env='AWS_SSO_ROLE_NAME',predictor='role'"`
+	Profile   string `kong:"short='p',help='Name of AWS Profile to assume',predictor='profile'"`
 
 	AccessKeyId     string `kong:"env='AWS_ACCESS_KEY_ID',hidden"`
 	SecretAccessKey string `kong:"env='AWS_SECRET_ACCESS_KEY',hidden"`
@@ -62,6 +63,15 @@ func (cc *ConsoleCmd) Run(ctx *RunContext) error {
 
 	if ctx.Cli.Console.Prompt {
 		return consolePrompt(ctx)
+	} else if ctx.Cli.Console.Profile != "" {
+		awssso := doAuth(ctx)
+		cache := ctx.Settings.Cache.GetSSO()
+		rFlat, err := cache.Roles.GetRoleByProfile(ctx.Cli.Console.Profile, ctx.Settings)
+		if err != nil {
+			return err
+		}
+
+		return openConsole(ctx, awssso, rFlat.AccountId, rFlat.RoleName)
 	} else if ctx.Cli.Console.Arn != "" {
 		awssso := doAuth(ctx)
 

cmd/eval_cmd.go
@@ -33,6 +33,7 @@ type EvalCmd struct {
 	Arn       string `kong:"short='a',help='ARN of role to assume',predictor='arn'"`
 	AccountId int64  `kong:"name='account',short='A',help='AWS AccountID of role to assume',predictor='accountId'"`
 	Role      string `kong:"short='R',help='Name of AWS Role to assume',predictor='role'"`
+	Profile   string `kong:"short='p',help='Name of AWS Profile to assume',predictor='profile'"`
 
 	Clear    bool   `kong:"short='c',help='Generate \"unset XXXX\" commands to clear environment'"`
 	NoRegion bool   `kong:"short='n',help='Do not set/clear AWS_DEFAULT_REGION from config.yaml'"`
@@ -56,19 +57,28 @@ func (cc *EvalCmd) Run(ctx *RunContext) error {
 
 	// refreshing?
 	if ctx.Cli.Eval.Refresh {
-		if len(ctx.Cli.Eval.EnvArn) == 0 {
+		if ctx.Cli.Eval.EnvArn != "" {
 			return fmt.Errorf("Unable to determine current IAM role")
 		}
 		accountid, role, err = utils.ParseRoleARN(ctx.Cli.Eval.EnvArn)
 		if err != nil {
 			return err
 		}
-	} else if len(ctx.Cli.Eval.Arn) > 0 {
+	} else if ctx.Cli.Eval.Profile != "" {
+		cache := ctx.Settings.Cache.GetSSO()
+		rFlat, err := cache.Roles.GetRoleByProfile(ctx.Cli.Eval.Profile, ctx.Settings)
+		if err != nil {
+			return err
+		}
+
+		role = rFlat.RoleName
+		accountid = rFlat.AccountId
+	} else if ctx.Cli.Eval.Arn != "" {
 		accountid, role, err = utils.ParseRoleARN(ctx.Cli.Eval.Arn)
 		if err != nil {
 			return err
 		}
-	} else if len(ctx.Cli.Eval.Role) > 0 && ctx.Cli.Eval.AccountId > 0 {
+	} else if ctx.Cli.Eval.Role != "" && ctx.Cli.Eval.AccountId > 0 {
 		// if CLI args are speecified, use that
 		role = ctx.Cli.Eval.Role
 		accountid = ctx.Cli.Eval.AccountId

cmd/exec_cmd.go
@@ -35,6 +35,7 @@ type ExecCmd struct {
 	Arn       string `kong:"short='a',help='ARN of role to assume',env='AWS_SSO_ROLE_ARN',predictor='arn'"`
 	AccountId int64  `kong:"name='account',short='A',help='AWS AccountID of role to assume',env='AWS_SSO_ACCOUNT_ID',predictor='accountId'"`
 	Role      string `kong:"short='R',help='Name of AWS Role to assume',env='AWS_SSO_ROLE_NAME',predictor='role'"`
+	Profile   string `kong:"short='p',help='Name of AWS Profile to assume',predictor='profile'"`
 	NoRegion  bool   `kong:"short='n',help='Do not set AWS_DEFAULT_REGION from config.yaml'"`
 
 	// Exec Params
@@ -54,7 +55,16 @@ func (cc *ExecCmd) Run(ctx *RunContext) error {
 	}
 
 	// Did user specify the ARN or account/role?
-	if ctx.Cli.Exec.Arn != "" {
+	if ctx.Cli.Exec.Profile != "" {
+		awssso := doAuth(ctx)
+		cache := ctx.Settings.Cache.GetSSO()
+		rFlat, err := cache.Roles.GetRoleByProfile(ctx.Cli.Exec.Profile, ctx.Settings)
+		if err != nil {
+			return err
+		}
+
+		return execCmd(ctx, awssso, rFlat.AccountId, rFlat.RoleName)
+	} else if ctx.Cli.Exec.Arn != "" {
 		awssso := doAuth(ctx)
 
 		accountid, role, err := utils.ParseRoleARN(ctx.Cli.Exec.Arn)

cmd/main.go
@@ -201,6 +201,7 @@ func parseArgs(cli *CLI) (*kong.Context, sso.OverrideSettings) {
 				"accountId": p.AccountComplete(),
 				"arn":       p.ArnComplete(),
 				"fieldList": p.FieldListComplete(),
+				"profile":   p.ProfileComplete(),
 				"region":    p.RegionComplete(),
 				"role":      p.RoleComplete(),
 				"sso":       p.SsoComplete(),

commit e5589e03db81056807d30382d8b2fe5a66d035c8

Fix condition
Error introduced by 41e96526:

- if len(role) == 0 || account == 0 {
+ if role != "" || account == 0 {

cmd/process_cmd.go
@@ -62,7 +62,7 @@ func (cc *ProcessCmd) Run(ctx *RunContext) error {
 		}
 	}
 
-	if role != "" || account == 0 {
+	if role == "" || account == 0 {
 		return fmt.Errorf("Please specify --arn or --account and --role")
 	}
 