9f2270dff1cc3586e63b6b4c76dc0ed78ed17b51
Aaron Turner <synfinatic@gmail.com>
2022-05-11 09:23:25 -0700

    Replace list --profile-prefix with --prefix
    Support more advanced prefix filter on any field, not just Profile
    with --prefix <FieldName>=<Prefix> instead of just
    --profile-prefix <Prefix> to enable more advanced auto-complete
    shell integrations that don't rely on bash completion.
    
    helpers now include `--no-config-check` by default
    
    Fixes: #395

--- internal/helper/bash_profile.sh
+++ internal/helper/bash_profile.sh
@@ -1,16 +1,16 @@
 __aws_sso_profile_complete() {
     COMPREPLY=()
-    local _args=${AWS_SSO_HELPER_ARGS:- -L error}
+    local _args=${AWS_SSO_HELPER_ARGS:- -L error --no-config-check}
     local cur
     _get_comp_words_by_ref -n : cur
 
-    COMPREPLY=($(compgen -W '$({{ .Executable }} $_args list --csv -P "$cur" Profile)' -- ""))
+    COMPREPLY=($(compgen -W '$({{ .Executable }} $_args list --csv -P "Profile=$cur" Profile)' -- ""))
 
     __ltrim_colon_completions "$cur"
 }
 
 aws-sso-profile() {
-    local _args=${AWS_SSO_HELPER_ARGS:- -L error}
+    local _args=${AWS_SSO_HELPER_ARGS:- -L error --no-config-check}
     if [ -n "$AWS_PROFILE" ]; then
         echo "Unable to assume a role while AWS_PROFILE is set"
         return 1
@@ -22,7 +22,7 @@ aws-sso-profile() {
 }
 
 aws-sso-clear() {
-    local _args=${AWS_SSO_HELPER_ARGS:- -L error}
+    local _args=${AWS_SSO_HELPER_ARGS:- -L error --no-config-check}
     if [ -z "$AWS_SSO_PROFILE" ]; then
         echo "AWS_SSO_PROFILE is not set"
         return 1
