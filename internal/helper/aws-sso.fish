513977c09841f3c13ec3b72150dc70b9eefc419b
zickzackv <fabian.otto@gmail.com>
2024-04-07 22:58:39 +0200

    fixed spelling

--- internal/helper/aws-sso.fish
+++ internal/helper/aws-sso.fish
@@ -23,7 +23,7 @@ function aws-sso-clear
   set --local _args (string split -- ' ' $AWS_SSO_HELPER_ARGS)
   set -q AWS_SSO_HELPER_ARGS; or set --local _args -L error
   if [ -z "$AWS_PROFILE" ]
-      echo "AWS_PROFILE ist not set"
+      echo "AWS_PROFILE is not set"
       return 1
   end
   eval "$({{ .Executable }} $_args eval -c | string replace "unset" "set --erase" )"
