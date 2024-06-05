e1210c9e08af2ae27482e49785fffbc8a3442739
drmikecrowe <drmikecrowe@gmail.com>
2024-06-05 05:16:11 -0400

    change aws-sso to {{ .Executable }}

--- internal/helper/aws-sso.fish
+++ internal/helper/aws-sso.fish
@@ -24,7 +24,7 @@ function __aws_sso_profile_complete
   set -q AWS_SSO_HELPER_ARGS; or set --local _args -L error --no-config-check
   set -l cur (commandline -t)
 
-  set -l cmd "aws-sso list $_args --csv -P Profile=$cur Profile"
+  set -l cmd "{{ .Executable }} list $_args --csv -P Profile=$cur Profile"
   for completion in (eval $cmd)
     printf "%s\n" $completion
   end
